276469a66af47a8971d9f0fc4b153c0e45b45f9a
George Fang <gyf2@duke.edu>
2023-10-18 21:19:07 +0200

    Made change for the text in the website

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Pee pee poo poo Tali
         </a>
       </header>
     </div>
