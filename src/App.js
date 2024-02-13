189c1b113aaabf34df3d5c795183c8e00b379b2c
Josh Rabbani <jar188@duke.edu>
2024-02-13 17:40:42 -0500

    testing

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import About from './components/About';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <About />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is a simple about page.</p>
+        </div>
+    );
+}
+
+export default About;
