9c06d9d48d2cb36f6cfc8b99d51732f3ea890e25
George Fang <gyf2@duke.edu>
2024-06-20 21:55:25 -0700

    Made sample change to see if website was set up right

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to George Fang's Website</h1>
+          <p>This is a simple React app deployed using GitHub Pages.</p>
+        </header>
+      </div>
   );
 }
 
